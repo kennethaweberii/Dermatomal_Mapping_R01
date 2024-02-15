2091eed078b4d452fed3378dcac182cc943d3e14
kennethaweberii <kenweber@stanford.edu>
2024-02-15 13:46:17 -0800

    fixed directory issue

--- biopac/create_aim_1_stmisola_stimulation_vector.py
+++ biopac/create_aim_1_stmisola_stimulation_vector.py
@@ -85,7 +85,7 @@ def main():
     phase=0
 
     #create time vector for stim_block
-    time=np.arange(0,args.stim_duration, 1/args.samp_f)+phase
+    #time=np.arange(0,args.stim_duration, 1/args.samp_f)+phase
     fsl_time=np.arange(0,args.stim_duration, 1/100)+phase #Using 100 Hz sampling frequency for fsl vector
 
     #Raise errors regarding pulse width
@@ -140,9 +140,9 @@ def main():
     plt.plot(np.arange(0,len(stim_vector)/args.samp_f, 1/args.samp_f), stim_vector, linewidth=0.001)
     plt.xlabel("Seconds")
     plt.ylabel("mA")
-    plt.savefig(os.path.join(args.save_directory, subject_id, subject_id + '_biopac_stim_vector_' + filename + '.pdf'))
+    plt.savefig(os.path.join(os.getcwd(), subject_id, subject_id + '_biopac_stim_vector_' + filename + '.pdf'))
     plt.close()
-    np.savetxt(os.path.join(args.save_directory, subject_id, subject_id + '_biopac_stim_vector_' + filename + '.txt'), stim_vector, fmt='%.1f\n', newline='')
+    np.savetxt(os.path.join(os.getcwd(), subject_id, subject_id + '_biopac_stim_vector_' + filename + '.txt'), stim_vector, fmt='%.1f\n', newline='')
 
     for stim_amp in np.arange(1, len(stim_amps)+1):
         fsl_vector = np.concatenate([np.arange(0,len(fsl_stim_vector)/100, 1/100).reshape((-1, 1)), (np.ones(len(fsl_stim_vector))/100).reshape(-1, 1), ((fsl_stim_vector == stim_amps[stim_amp - 1])*1).reshape(-1, 1)], axis=1)
@@ -150,9 +150,9 @@ def main():
         plt.plot(np.arange(0,len(fsl_stim_vector)/100, 1/100), ((fsl_stim_vector == stim_amps[stim_amp - 1])*1), linewidth=0.001)  #Using 100 Hz sampling frequency for fsl vector
         plt.xlabel("Seconds")
         plt.ylabel("AU")
-        plt.savefig(os.path.join(args.save_directory, subject_id, 'fsl_stim_vectors', subject_id + '_fsl_vector_' + filename + '_stim_amp_' + str(stim_amp) + '.pdf'))
+        plt.savefig(os.path.join(os.getcwd(), subject_id, 'fsl_stim_vectors', subject_id + '_fsl_vector_' + filename + '_stim_amp_' + str(stim_amp) + '.pdf'))
         plt.close()
-        np.savetxt(os.path.join(args.save_directory, subject_id, 'fsl_stim_vectors', subject_id + '_fsl_vector_' + filename + '_stim_amp_' + str(stim_amp) + '.txt'), fsl_vector, fmt='%.2f\t%.2f\t%d\n', newline='')
+        np.savetxt(os.path.join(os.getcwd(), subject_id, 'fsl_stim_vectors', subject_id + '_fsl_vector_' + filename + '_stim_amp_' + str(stim_amp) + '.txt'), fsl_vector, fmt='%.2f\t%.2f\t%d\n', newline='')
         
         for stim_index in np.arange(1,len(np.where((fsl_vector[:,2][:-1]==0) & (fsl_vector[:,2][1:]==1))[0])+1):
             stim_starts = np.where((fsl_vector[:,2][:-1]==0) & (fsl_vector[:,2][1:]==1))[0] + 1
@@ -166,9 +166,9 @@ def main():
             plt.plot(np.arange(0,len(fsl_stim_vector)/100, 1/100), fsl_single_stim_vector[:,2], linewidth=0.001)  #Using 100 Hz sampling frequency for fsl vector
             plt.xlabel("Seconds")
             plt.ylabel("AU")
-            plt.savefig(os.path.join(args.save_directory, subject_id, 'fsl_stim_vectors', subject_id + '_fsl_vector_' + filename + '_stim_amp_' + str(stim_amp) + '_stim_' + str(stim_index) + '.pdf'))
+            plt.savefig(os.path.join(os.getcwd(), subject_id, 'fsl_stim_vectors', subject_id + '_fsl_vector_' + filename + '_stim_amp_' + str(stim_amp) + '_stim_' + str(stim_index) + '.pdf'))
             plt.close()
-            np.savetxt(os.path.join(args.save_directory, subject_id, 'fsl_stim_vectors', subject_id + '_fsl_vector_' + filename + '_stim_amp_' + str(stim_amp) + '_stim_' + str(stim_index) + '.txt'), fsl_single_stim_vector, fmt='%.2f\t%.2f\t%d\n', newline='')
+            np.savetxt(os.path.join(os.getcwd(), subject_id, 'fsl_stim_vectors', subject_id + '_fsl_vector_' + filename + '_stim_amp_' + str(stim_amp) + '_stim_' + str(stim_index) + '.txt'), fsl_single_stim_vector, fmt='%.2f\t%.2f\t%d\n', newline='')
 
 if __name__ == '__main__':
     main()
