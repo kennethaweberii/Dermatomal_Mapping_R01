af86d66aafa7e165c7cf0e7170139ac4b7278d69
sandrinebedard <sandrine.bedard@polymtl.ca>
2024-04-04 10:24:23 -0400

    comment dummy volumes

--- preprocessing/preprocess_spinal_cord.sh
+++ preprocessing/preprocess_spinal_cord.sh
@@ -270,7 +270,7 @@ if [[ $SES == *"spinalcord"* ]];then
           mkdir -p ${PATH_DATA_PROCESSED}/${SUBJECT}/func/run-${run}/PNM_run-${run}/
 
           # Remove dummy volumes
-          fslroi ${file_task} ${file_task} 2 -1
+          #fslroi ${file_task} ${file_task} 2 -1
               
           # Get dims
           number_of_volumes=$(fslval ${file_task} dim4)
