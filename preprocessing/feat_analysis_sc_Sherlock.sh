f3d711bba90bd4914fe0859b65ba5f8492c16dc7
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-03-21 12:31:53 -0700

    fix script path

--- preprocessing/feat_analysis_sc_Sherlock.sh
+++ preprocessing/feat_analysis_sc_Sherlock.sh
@@ -5,6 +5,7 @@
 data_path=/home/sbedard/Projects/Dermatomal_Mapping_R01
 scripts_path=${HOME}/codes/Dermatomal_Mapping_R01/preprocessing
 
+
 subjects=(sub-DMAim1HCHC004_testing_sherlock)
 
 regions=(spinalcord)
@@ -27,7 +28,7 @@ for run in ${runs[@]}; do  #consider moving this loop to other
             fi
 
             export data_path scripts_path SCRATCH subject region run time_limit memory smoothing
-            envsubst '${data_path} ${scripts_path} ${SCRATCH} ${subject} ${region} ${run} ${time_limit} ${memory} ${smoothing}' < ${scripts_path}/preprocessing/feat_analysis_sc_Sherlock.sbatch > feat_analysis_sc_Sherlock_${subject}_${region}_${task}.sbatch
+            envsubst '${data_path} ${scripts_path} ${SCRATCH} ${subject} ${region} ${run} ${time_limit} ${memory} ${smoothing}' < ${scripts_path}/feat_analysis_sc_Sherlock.sbatch > feat_analysis_sc_Sherlock_${subject}_${region}_${task}.sbatch
             sbatch feat_analysis_sc_Sherlock_${subject}_${region}_${run}.sbatch
             rm feat_analysis_sc_Sherlock_${subject}_${region}_${run}.sbatch
             # TODO: run average
