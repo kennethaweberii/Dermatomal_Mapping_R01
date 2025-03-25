9c6b2e0208b08c5118c854e67cfd1ccbde806de5
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-03-25 12:32:52 -0700

    fix task name

--- preprocessing/feat_analysis_sc_Sherlock.sh
+++ preprocessing/feat_analysis_sc_Sherlock.sh
@@ -10,10 +10,10 @@ subjects=(sub-DMAim1HCHC004_testing_sherlock)
 
 regions=(spinalcord)
 runs=(1 2 3)
-runs=(2 3)
+runs=(2)
 
-for run in ${runs[@]}; do  #consider moving this loop to other
-    for subject in ${subjects[@]}; do
+for subject in ${subjects[@]}; do
+    for run in ${runs[@]}; do  #consider moving this loop to other
         for region in ${regions[@]}; do
 
             echo ${subject} ${region} "run " ${run}
@@ -32,9 +32,18 @@ for run in ${runs[@]}; do  #consider moving this loop to other
             envsubst '${data_path} ${scripts_path} ${SCRATCH} ${subject} ${region} ${run} ${time_limit} ${memory} ${smoothing}' < ${scripts_path}/feat_analysis_sc_Sherlock.sbatch > feat_analysis_sc_Sherlock_${subject}_${region}_${run}.sbatch
             sbatch feat_analysis_sc_Sherlock_${subject}_${region}_${run}.sbatch
             rm feat_analysis_sc_Sherlock_${subject}_${region}_${run}.sbatch
-            # TODO: run average
             sleep 10s
 
         done
+        # # Run average across runs
+        # time_limit=08:00:00
+        # memory=16000
+        # smoothing=0
+        # export data_path scripts_path SCRATCH subject region run time_limit memory smoothing
+        # envsubst '${data_path} ${scripts_path} ${SCRATCH} ${subject} ${region} ${run} ${time_limit} ${memory} ${smoothing}' < ${scripts_path}/feat_analysis_sc_Sherlock_average.sbatch > feat_analysis_sc_Sherlock_${subject}_${region}_${run}_average.sbatch
+        # sbatch feat_analysis_sc_Sherlock_${subject}_${region}_${run}_average.sbatch
+        # rm feat_analysis_sc_Sherlock_${subject}_${region}_${run}_average.sbatch
+        # sleep 10s
+
     done
 done
