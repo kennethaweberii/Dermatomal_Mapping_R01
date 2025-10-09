2c08a6078e3fc4fce940829b899df00666a47206
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-10-09 13:20:24 -0700

    fix path

--- sample_size/cp_data_2_sherlock.sh
+++ sample_size/cp_data_2_sherlock.sh
@@ -12,9 +12,9 @@ subjects=($(< "${path_script}/include_n40.txt"))
 
 for subject in "${subjects[@]}"; do
     echo "Copying data for subject: $subject"
-    mkdir -p ${path_out}/${subject}/ses-ses-spinalcord/func/
-    cd ${path_out}/${subject}/ses-ses-spinalcord/func/
-    file_path="${path_in}/${subject}/ses-ses-spinalcord/func/${subject}_ses-spinalcord_task-tens_run-average_bold_mc2_pnm_stc2template_smooth225_trialwise_second_level.gfeat"
+    mkdir -p ${path_out}/${subject}/ses-spinalcord/func/
+    cd ${path_out}/${subject}/ses-spinalcord/func/
+    file_path="${path_in}/${subject}/ses-spinalcord/func/${subject}_ses-spinalcord_task-tens_run-average_bold_mc2_pnm_stc2template_smooth225_trialwise_second_level.gfeat"
     #Copy functional
     ret=1
     c=0
