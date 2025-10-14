e03366c7007e9e9daf69d4f7dc5151c9b9f279b4
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-10-14 12:39:38 -0700

    fix exporting subjects

--- sample_size/feat_group_analysis_sample_size_sherlock.sh
+++ sample_size/feat_group_analysis_sample_size_sherlock.sh
@@ -18,9 +18,12 @@ for n in "${n_sample[@]}"; do
         sub_list=${path_list}/sample_${n}/include_sample_${n}_${iter}.txt
 
         output_path=${path_data}/sample_size/n${n}/sample_size_${n}_iter_${iter}
-        mkdir -p ${output_path}
+        #mkdir -p ${output_path}
         subjects=($(< ${sub_list}))
         echo "${subjects[@]}"
+        for i in "${!subjects[@]}"; do
+            export sub${i}="${subjects[$i]}"
+        done
         export path_script path_data SCRATCH time_limit memory output_path subjects n iter
         envsubst '${path_data} ${path_script} ${SCRATCH} ${time_limit} ${memory} ${output_path} ${subjects} ${n} ${iter}' < ${path_script}/feat_group_analysis_sample_size_sherlock.sbatch > feat_group_analysis_sample_size_sherlock_n${n}_iter${iter}.sbatch
         sbatch feat_group_analysis_sample_size_sherlock_n${n}_iter${iter}.sbatch
