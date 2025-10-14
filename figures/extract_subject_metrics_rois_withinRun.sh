2d344ef10ba908643e75a195ed6115cef630a7d5
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-10-14 12:19:48 -0700

    change run

--- figures/extract_subject_metrics_rois_withinRun.sh
+++ figures/extract_subject_metrics_rois_withinRun.sh
@@ -12,8 +12,8 @@ amp=4
 # cope 2: 19 - 36
 # cope 3: 37 - 54
 # cope 4: 55 - 72
-runs=(2)
-run=2
+runs=(3)
+run=3
 subjects=(sub-DMAim1HC004 sub-DMAim1HC006 sub-DMAim1HC007 sub-DMAim1HC015 sub-DMAim1HC018 sub-DMAim1HC020 sub-DMAim1HC030 sub-DMAim1HC033 sub-DMAim1HC001 sub-DMAim1HC005 sub-DMAim1HC016 sub-DMAim1HC017 sub-DMAim1HC022 sub-DMAim1HC023 sub-DMAim1HC027 sub-DMAim1HC025 sub-DMAim1HC035 sub-DMAim1HC008 sub-DMAim1HC031 sub-DMAim1HC032 sub-DMAim1HC034 sub-DMAim1HC019 sub-DMAim1HC037 sub-DMAim1HC028 sub-DMAim1HC038 sub-DMAim1HC039 sub-DMAim1HC009 sub-DMAim1HC045 sub-DMAim1HC002 sub-DMAim1HC047 sub-DMAim1HC029 sub-DMAim1HC036 sub-DMAim1HC046 sub-DMAim1HC042 sub-DMAim1HC003 sub-DMAim1HC053 sub-DMAim1HC052 sub-DMAim1HC051 sub-DMAim1HC054 sub-DMAim1HC013)
 rois=(left_sc_gm_mask right_sc_gm_mask left_sc_mask right_sc_mask C6_left_sc_gm_mask C6_right_sc_gm_mask C7_left_sc_gm_mask C7_right_sc_gm_mask C8_left_sc_gm_mask C8_right_sc_gm_mask) # left_sc_vh_mask right_sc_vh_mask left_sc_vq_mask right_sc_vq_mask)
 #rois=(left_sc_mask right_sc_mask left_vh_mask right_vh_mask left_vq_mask right_vq_mask)
