34774e67d3674a9f248c757ca2464c30442a2e4c
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-10-23 11:34:07 -0700

    add copying data for run 1

--- sample_size/cp_data_2_sherlock.sh
+++ sample_size/cp_data_2_sherlock.sh
@@ -4,7 +4,7 @@
 path_in=/home/sbedard/Projects/Dermatomal_Mapping_R01/data/BIDS/derivatives
 path_out=${SCRATCH}/dermatomal_mapping_R01/derivatives
 path_script=~/codes/Dermatomal_Mapping_R01
-
+run="1"  # Or average
 
 #Read include list yaml file
 # TODO: check is installed
@@ -14,7 +14,12 @@ for subject in "${subjects[@]}"; do
     echo "Copying data for subject: $subject"
     mkdir -p ${path_out}/${subject}/ses-spinalcord/func/
     cd ${path_out}/${subject}/ses-spinalcord/func/
-    file_path="${path_in}/${subject}/ses-spinalcord/func/${subject}_ses-spinalcord_task-tens_run-average_bold_mc2_pnm_stc2template_smooth225_trialwise_second_level.gfeat"
+    if [ "${run}" = "average" ]; then
+        file_path="${path_in}/${subject}/ses-spinalcord/func/${subject}_ses-spinalcord_task-tens_run-${run}_bold_mc2_pnm_stc2template_smooth225_trialwise_second_level.gfeat"
+    else
+        mkdir -p ${path_out}/${subject}/ses-spinalcord/func/run-${run}/
+        cd run-${run}/
+        file_path="${path_in}/${subject}/ses-spinalcord/func/run-${run}/${subject}_ses-spinalcord_task-tens_run-${run}_bold_mc2_pnm_stc2template_smooth225_trialwise_second_level.gfeat"
     #Copy functional
     ret=1
     c=0
