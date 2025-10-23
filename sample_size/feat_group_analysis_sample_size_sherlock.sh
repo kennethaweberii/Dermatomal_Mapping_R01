72522374d422ec26b7efd95568ff3a9368043090
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-10-23 13:36:22 -0700

    update to be able to run with run-1 too

--- sample_size/feat_group_analysis_sample_size_sherlock.sh
+++ sample_size/feat_group_analysis_sample_size_sherlock.sh
@@ -4,6 +4,7 @@
 path_script=~/codes/Dermatomal_Mapping_R01/sample_size/
 path_data=${SCRATCH}/dermatomal_mapping_R01/derivatives
 path_list=${SCRATCH}/dermatomal_mapping_R01/lists/
+run="1"  # Or average
 iterations=1000
 #iterations=1
 #n_sample=(10 15 20 25 30 35)
@@ -19,15 +20,15 @@ for n in "${n_sample[@]}"; do
     for iter in $(seq 1 $iterations); do
         sub_list=${path_list}/sample_${n}/include_sample_n${n}_${iter}.txt
 
-        output_path=${path_data}/sample_size/n${n}/sample_size_${n}_iter_${iter}
+        output_path=${path_data}/sample_size/run-${run}/n${n}/sample_size_${n}_iter_${iter}
         #mkdir -p ${output_path}
         subjects=($(< ${sub_list}))
         echo "${subjects[@]}"
         for i in "${!subjects[@]}"; do
             export sub${i}="${subjects[$i]}"
         done
-        export path_script path_data SCRATCH time_limit memory output_path subjects n iter
-        envsubst '${path_data} ${path_script} ${SCRATCH} ${time_limit} ${memory} ${output_path} ${subjects} ${n} ${iter}' < ${path_script}/feat_group_analysis_sample_size_sherlock.sbatch > feat_group_analysis_sample_size_sherlock_n${n}_iter${iter}.sbatch
+        export path_script path_data SCRATCH time_limit memory output_path subjects n iter run
+        envsubst '${path_data} ${path_script} ${SCRATCH} ${time_limit} ${memory} ${output_path} ${subjects} ${n} ${iter} ${run}' < ${path_script}/feat_group_analysis_sample_size_sherlock.sbatch > feat_group_analysis_sample_size_sherlock_n${n}_iter${iter}.sbatch
         sbatch feat_group_analysis_sample_size_sherlock_n${n}_iter${iter}.sbatch
         rm feat_group_analysis_sample_size_sherlock_n${n}_iter${iter}.sbatch
         sleep 10s
